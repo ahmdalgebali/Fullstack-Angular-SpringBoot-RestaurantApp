eaf02f9f31f3dbb0c9d6685394c6ffea3f0f0a01
ahmdalgebali <ahmedelgebalywork@gmail.com>
2022-11-06 19:01:31 +0200

    Add starter-files

---
+++ 02-backend/spring-Resturant/src/main/java/com/jabal/spring/restaurant/SpringResturantApplication.java
@@ -0,0 +1,13 @@
+package com.jabal.spring.restaurant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringResturantApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringResturantApplication.class, args);
+    }
+
+}
